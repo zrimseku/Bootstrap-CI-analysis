cee836489821e86873245133a52187ec78ca5856
TURBOPC\ursau <uz2273@student.uni-lj.si>
2022-07-14 18:48:03 +0200

    Lengths and shapes of intervals

--- bootstrap-ci.py
+++ bootstrap-ci.py
@@ -227,7 +227,7 @@ class CompareIntervals:
         for method in self.methods:
             for alpha in self.alphas:
                 # ce zelimo dodatne (klasicne) metode en if stavek tuki
-                self.computed_intervals[method][alpha].append(bts.ci(coverage=alpha, side='one', method=method))
+                self.computed_intervals[method][alpha].append(bts.ci(coverage=alpha, side='one', method=method)[0])
             # print('finished', method)
         return bts
 
@@ -246,7 +246,7 @@ class CompareIntervals:
         bts = self.compute_intervals(data)
 
         # plotting
-        colors = iter(plt.cm.jet(np.linspace(0.05, 0.95, len(self.methods))))
+        colors = iter(plt.cm.jet(np.linspace(0, 0.95, len(self.methods))))
         plt.hist(bts.statistic_values, bins=30, label='statistic')
         if 'smoothed' in self.methods:
             plt.hist(bts.statistic_values_noise, bins=30, label='smoothed statistic', alpha=0.3)
@@ -254,26 +254,53 @@ class CompareIntervals:
             col = next(colors)
             for alpha in alphas_to_draw:
                 if alpha == alphas_to_draw[0]:  # label only the first line of a method to avoid duplicates in legend
-                    plt.axvline(self.computed_intervals[method][alpha][-1], linestyle='--', label=method, color=col)
+                    plt.axvline(self.computed_intervals[method][alpha][-1], linestyle='--', label=method, color=col,
+                                alpha=0.8)
                 else:
-                    plt.axvline(self.computed_intervals[method][alpha][-1], linestyle='--', color=col)
+                    plt.axvline(self.computed_intervals[method][alpha][-1], linestyle='--', color=col, alpha=0.8)
 
         plt.legend()
         plt.show()
 
-    def compare_coverages(self, repetitions, true_statistic_value=None):
+    def compare_intervals(self, repetitions, true_statistic_value=None, length=None):
         if true_statistic_value is None:
             # compute true statistic value from exact simulation, if it is not given
-            # a je to vedno pravilno?
             true_statistic_value = self.exact_interval_simulation(repetitions)
 
-        for r in tqdm(range(repetitions)):
+        stat_original = []
+        for _ in tqdm(range(repetitions)):
             data = self.dgp(self.n)
-            self.compute_intervals(data)
+            bts = self.compute_intervals(data)
+            stat_original.append(bts.original_statistic_value)
+        stat_original = np.array(stat_original)
 
         coverages = {method: {alpha: np.mean(np.array(self.computed_intervals[method][alpha][-repetitions:]) >
                                              true_statistic_value) for alpha in self.alphas} for method in self.methods}
-        return coverages
+
+        if length is not None:
+            low_alpha, high_alpha = [(1-length)/2, (length+1)/2]
+            if low_alpha not in self.alphas or high_alpha not in self.alphas:
+                assert ValueError(f"Length of {length} CI can't be calculated, because we don't have calculations for"
+                                  f"corresponding alphas.")
+        else:
+            low_alpha, high_alpha = [min(self.alphas), max(self.alphas)]
+            print(f'Calculating lengths of {high_alpha - low_alpha} CI, from {low_alpha} to {high_alpha} quantiles.')
+
+        lengths = {method: np.array(self.computed_intervals[method][high_alpha][-repetitions:]) -
+                           np.array(self.computed_intervals[method][low_alpha][-repetitions:])
+                   for method in self.methods}
+
+        length_stats = {method: {'mean': np.mean(lengths[method]), 'std': np.std(lengths[method])}
+                        for method in self.methods}
+
+        shapes = {method: (np.array(self.computed_intervals[method][high_alpha][-repetitions:]) - stat_original) /
+                          (stat_original - np.array(self.computed_intervals[method][low_alpha][-repetitions:]))
+                  for method in self.methods}
+
+        shape_stats = {method: {'mean': np.mean(shapes[method]), 'std': np.std(shapes[method])}
+                       for method in self.methods}
+
+        return coverages, length_stats, shape_stats
 
 
 def compare_bootstraps_with_library_implementations(data, statistic, methods, B, alpha):
@@ -344,7 +371,7 @@ if __name__ == '__main__':
     # INTERVAL COMPARISON
     alphas = [0.05, 0.1, 0.5, 0.9, 0.95]
 
-    statistic = np.median
+    statistic = np.mean
     true_stat_value = 3
     par2 = 2
 
@@ -355,6 +382,8 @@ if __name__ == '__main__':
         return np.random.gamma(shape=true_stat_value/par2, scale=par2, size=x)
 
     comparison = CompareIntervals(statistic, methods, dgp_gamma, n, B, alphas)
-    # print(comparison.compare_coverages(repetitions=100, true_statistic_value=true_stat_value))
+    for c in comparison.compare_intervals(repetitions=100, true_statistic_value=true_stat_value):
+        print(c)
 
     comparison.draw_intervals([0.1, 0.9])
+
