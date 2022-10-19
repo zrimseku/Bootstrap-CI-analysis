764f79905470201f7b81d77c352982ca5bfb7ccb
zrimseku <uz2273@student.uni-lj.si>
2022-10-19 10:03:57 +0200

    set_ylim

--- results_visualizations.py
+++ results_visualizations.py
@@ -179,9 +179,10 @@ def main_plot_comparison(B_as_method=False, filter_by={}, additional='', scale='
                         print(statistic, B)
                         continue
                     title = f'{comparing}s for {statistic} using B = {B}'
+                    subfolder = '' if set_ylim else 'noylim'
                     compare_cov_dis_grid(df_part, comparing=comparing, filter_by=filter_by, x='n', row='alpha',
                                          col='dgp', title=title, save_add=f'{statistic}_{B}{additional}', scale=scale,
-                                         folder_add=folder_add, set_ylim=set_ylim)
+                                         folder_add=folder_add, set_ylim=set_ylim, subfolder=subfolder)
         del df
 
 
@@ -234,7 +235,7 @@ if __name__ == '__main__':
     cov = pd.read_csv(f'results{folder_add}/coverage.csv')
     bts_methods = ['percentile', 'standard', 'basic', 'bc', 'bca', 'double', 'smoothed']
 
-    main_plot_comparison(filter_by={}, additional=additional, scale='linear', folder_add=folder_add)
+    main_plot_comparison(filter_by={}, additional=additional, scale='linear', folder_add=folder_add, set_ylim=False)
 
     # plot_times_lengths_grid('length', scale='linear', folder_add=folder_add, save_add=additional)
     # plot_times_lengths_grid('times', scale='linear', folder_add=folder_add, save_add=additional)
